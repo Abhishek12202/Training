eb6ee632f33cff3ca8bef57a3c99e66dbdb95772
dd <dd>
2021-04-02 15:57:23 +0530

    dd

--- src/ContextAPI/App2.js
+++ src/ContextAPI/App2.js
@@ -1,10 +1,8 @@
 import React from 'react'
-import App3 from './App3'
 
 const App2 = () => {
     return (
         <div>
-          <App3/>  
         </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './ContextAPI/App';
 import DynamicInput from './D_input2/DynamicInput';
 
-ReactDOM.render(<DynamicInput/>,document.getElementById('root'));
+ReactDOM.render(<App/>,document.getElementById('root'));
 
